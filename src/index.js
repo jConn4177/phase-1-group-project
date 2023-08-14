e231652d532f4a8bfee7c1910ae69aa11e837df6
Vanessa Jesik <vajesik@gmail.com>
2023-08-14 13:37:33 -0600

    fetch request for all plants

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 //Initial GET request from db.json file
+fetch("http://localhost:3000/plants")
+  .then(response => response.json())
+  .then(plants => createPlantList(plants));
+
+//createPlantList function creates a div for each plant
+function createPlantList(plants) {
+  console.log("plants");
+}
