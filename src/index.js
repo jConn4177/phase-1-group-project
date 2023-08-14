5af6f37ce7e4fd971f4f3f6cc721130c04f86637
Vanessa Jesik <vajesik@gmail.com>
2023-08-14 16:24:44 -0600

    added error catch to GET request and reordered JS file

--- src/index.js
+++ src/index.js
@@ -1,48 +1,14 @@
-
-// Add plant form with even listener
-const newForm = document.querySelector('#new-plant')
-newForm.addEventListener('submit', handleSubmit)
-
-// Create object for new plants
-function handleSubmit(e) {
-    e.preventDefault()
-    const newPlant = {
-    name: newForm.name.value,
-    image: newForm.image.value,
-    description: newForm.description.value,
-    favorite: document.querySelector('#new-favorite').checked
-    }
-
-    //postJSON(newPlant)
-    console.log(newPlant)
-    newForm.reset()
-}
-
-/*
-// Post new plant to server
-const postJSON = (data, url = 'http://localhost:3000/plants/') => {
-    return fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json", Accept: "application/json" },
-      body: JSON.stringify(data),
-    }).then((response) => {
-      if (!response.ok) {
-        throw response.statusText;
-      }
-      return response.json();
-    });
-  };
-  */
-
 //Initial GET request from db.json file
 fetch("http://localhost:3000/plants")
   .then(response => response.json())
-  .then(plants => createPlantList(plants));
+  .then(plants => createPlantList(plants))
+  .catch(error => alert("You forgot to set up your server!"));
 
 const plantListDiv = document.getElementById("plant-list");
 
 //createPlantList function creates a div for each plant
 //Adds mouseover event to each plantListName div
+//Adds click event to each plantListName div
 function createPlantList(plants) {
   plants.forEach(plant => {
     const plantListName = document.createElement("div");
@@ -64,3 +30,37 @@ function createPlantList(plants) {
 //Card has a Delete button
 function displayPlantCard() {}
 
+// Add plant form with event listener
+const newForm = document.querySelector("#new-plant");
+newForm.addEventListener("submit", handleSubmit);
+
+// Create object for new plants
+function handleSubmit(e) {
+  e.preventDefault();
+  const newPlant = {
+    name: newForm.name.value,
+    image: newForm.image.value,
+    description: newForm.description.value,
+    favorite: document.querySelector("#new-favorite").checked,
+  };
+
+  //postJSON(newPlant)
+  console.log(newPlant);
+  newForm.reset();
+}
+
+/*
+// Post new plant to server
+const postJSON = (data, url = 'http://localhost:3000/plants/') => {
+    return fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json", Accept: "application/json" },
+      body: JSON.stringify(data),
+    }).then((response) => {
+      if (!response.ok) {
+        throw response.statusText;
+      }
+      return response.json();
+    });
+  };
+  */
