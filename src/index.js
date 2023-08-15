9615ad559e465f44a7cc9978861676c406138e51
Vanessa Jesik <vajesik@gmail.com>
2023-08-15 14:11:55 -0600

    style of plant list

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ let currentPlant; //* necessary for patch
 
 //*Initial GET request from db.json file
 fetch(url)
-  .then((response) => response.json())
-  .then((plants) => {
+  .then(response => response.json())
+  .then(plants => {
     isFavoriteTrue = Boolean(plants[0].favorite); //* Sets favorite button default
     updateButtonDisplay(isFavoriteTrue); //*Sets favorite button
     displayPlantCard(plants[0]); //*Sets first plant as default card
     createPlantList(plants);
     currentPlant = plants[0];
   })
-  .catch((error) => alert("You forgot to set up your server!"));
+  .catch(error => alert("You forgot to set up your server!"));
 
 const plantListDiv = document.getElementById("plant-list");
 
@@ -20,23 +20,24 @@ const plantListDiv = document.getElementById("plant-list");
 //*Adds mouseover event to each plantListName div
 //*Adds click event to each plantListName div
 function createPlantList(plants) {
-  plants.forEach((plant) => {
+  plants.forEach(plant => {
     const plantListName = document.createElement("div");
     plantListName.textContent = plant.name;
-    plantListName.addEventListener("mouseover", (event) => {
+    plantListName.classList.add("list-style");
+    plantListName.addEventListener("mouseover", event => {
       console.log("mouseover");
     });
-    plantListName.addEventListener("click", (event) => {
+    plantListName.addEventListener("click", event => {
       isFavoriteTrue = Boolean(plant.favorite); //* sets favorite button textContent
       currentPlant = plant;
-      displayPlantCard();
+      displayPlantCard(plant);
     });
     plantListDiv.append(plantListName);
   });
 }
 
 //*Renders Plant Card Display
-const displayPlantCard = (plant) => {
+const displayPlantCard = plant => {
   const plantName = document.querySelector("#plant-name");
   plantName.textContent = plant.name;
   const plantImage = document.querySelector("#plant-image");
@@ -102,7 +103,7 @@ function updateButtonDisplay(isTrue) {
 }
 
 //* Updates the favorite
-const updateFavorite = (plantObj) => {
+const updateFavorite = plantObj => {
   const updatedPlant = { ...plantObj };
   updatedPlant.favorite = !updatedPlant.favorite;
   patchJSON(url + `/${plantObj.id}`, { favorite: updatedPlant.favorite }).then(
@@ -114,10 +115,10 @@ const updateFavorite = (plantObj) => {
 };
 
 //* Deletes a plant from db.json
-const deletePlant = (plantObj) => {
-  getJSON(url).then((plants) => {
+const deletePlant = plantObj => {
+  getJSON(url).then(plants => {
     deleteJSON(url + `/${plantObj.id}`);
-    const plantIndex = plants.findIndex((plant) => plant.id === plantObj.id);
+    const plantIndex = plants.findIndex(plant => plant.id === plantObj.id);
     if (plantIndex !== -1 && plantIndex < plants.length - 1) {
       currentPlant = plants[plantIndex + 1];
       displayPlantCard(plants[plantIndex + 1]);
